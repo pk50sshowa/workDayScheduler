2e13ce13e3848d6b3b2d763024a1035360bafed5
Paul Koehler <paul.koehler@gmail.com>
2023-03-27 22:11:02 -0700

    all files changed, functions tested and ready to go pending final screenshot upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# Work Scheduler
+
+## Description
+
+An application that gives users the opportunity to enter events into individual time blocks from 9 a.m. to 5 p.m. and save those events into local storage. Time blocks are dynamically updated to show whether they are in the past, present, or future for that particular day.
+
+## Location
+
+The site is live at the following URL:
+[https://pk50sshowa.github.io/workDayScheduler/index.html](https://pk50sshowa.github.io/pworkDayScheduler/index.html)
+
+## Screenshots
+
+![Screenshot 1](screenshot1.jpg)
+
+## Developer Comments
+
+Prompts for the password length and desired character sets are given through window alerts.
+The generated password itself is also shown through a window alert is written to the main window.
+
+## Contributing
+
+Please take a look at the code history at this project's repo page:
+[https://github.com/pk50sshowa/workDayScheduler/](https://github.com/pk50sshowa/workDayScheduler/)
+
+## Support
+
+Any questions can be directed to the developer at paul.koehler@gmail.com
+
+## Credits
+
+jQuery may be legacy code at this point, but I'm grateful to the people out there who helped walk me through some of the steps related to local storage, especially Meg Meyers!
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Initial loop to populate timeBlocks with local storage items if they exist
+for (var i = 9; i < 18; i++) {
+    $('#hour-' + i + ' .description').val(localStorage.getItem('hour-' + i));
+}
 
+// Global variables for the application and time display
 var saveButton = $('.saveBtn');
-var textArea = $('description');
 
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd MMMM D, YYYY h:mm A'));
 
+// timeBlocks are to represent each hour of the day (for 9 a.m. to 5 p.m. in this application) and dynamically change class depending on the specific time of day
 var currentTime = dayjs().hour();
 var timeBlocks = $('.time-block');
 
-// for (i = 0; i < timeBlocks.length; i++) {
-//   console.log(timeBlocks[i].id);
-// }
-
+// Specific timeBlock function
 timeBlocks.each(function () {
   console.log(this.id);
   var blockID = parseInt(this.id.split('-')[1]);
   console.log(blockID);
 
   if (blockID < currentTime) {
+    $(this).removeClass('present');
+    $(this).removeClass('future');
     $(this).addClass('past');
   } else if (blockID === currentTime) {
+    $(this).removeClass('past');
+    $(this).removeClass('future');
     $(this).addClass('present');
   } else {
+    $(this).removeClass('present');
+    $(this).removeClass('past');
     $(this).addClass('future');
   }
 });
 
+// Save button to store inserted task to local storage along with its associated time ID
 saveButton.on('click', function () {
- 
-    // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+var hourEl = $(this).parent().attr('id');
+var descrEl = $(this).siblings('.description').val();
+localStorage.setItem(hourEl, descrEl);
 
-    var thisTextArea = this.textArea;
-    localStorage.setItem(thisTextArea, JSON.stringify(thisTextArea));
-    console.log(thisTextArea);
-
-
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-  // TBD
-  
+console.log(hourEl, descrEl);
 });
